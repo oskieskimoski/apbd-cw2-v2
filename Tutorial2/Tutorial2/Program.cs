bed64ec22b87ab1221ea746e288639f022d88e62
s33652 <oski.eskimoski@wp.pl>
2026-03-12 11:34:40 +0100

    StatisticsHelper testing

--- Tutorial2/Tutorial2/Program.cs
+++ Tutorial2/Tutorial2/Program.cs
@@ -1,3 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using Tutorial2;
+
+StatisticsHelper s = new StatisticsHelper();
+
+Console.WriteLine("StatisticsHelper created");
+
+
