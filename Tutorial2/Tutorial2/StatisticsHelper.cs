12d4a9b8bf773af24201e94f4c09ab6fbb179b1d
s33652 <oski.eskimoski@wp.pl>
2026-03-12 11:06:43 +0100

    Class StatisticsHelper added

---
+++ Tutorial2/Tutorial2/StatisticsHelper.cs
@@ -0,0 +1,20 @@
+﻿namespace Tutorial2;
+
+public class StatisticsHelper
+{
+    private int a = 0; 
+    private int b = 0;
+
+
+    static StatisticsHelper()
+    {
+        
+    }
+
+
+
+
+
+
+
+}
