088d0960a7487d082875d26666f3a20c46b21d6e
s33652 <oski.eskimoski@wp.pl>
2026-03-12 11:22:35 +0100

    StatisticsHelper Constructor created

--- Tutorial2/Tutorial2/StatisticsHelper.cs
+++ Tutorial2/Tutorial2/StatisticsHelper.cs
@@ -4,12 +4,13 @@ public class StatisticsHelper
 {
     private int a = 0; 
     private int b = 0;
-
-
+    
     static StatisticsHelper()
     {
-        
+        int a = int.Parse( Console.ReadLine());
+        int b = int.Parse( Console.ReadLine());
     }
+    
 
 
 
