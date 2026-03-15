35e98b2cadd7510d80fca2539230e8a811ec4e33
oskieskimoski <57591991+oskieskimoski@users.noreply.github.com>
2026-03-15 18:05:56 +0100

    Array val renamed

--- Tutorial2/Tutorial2/Program.cs
+++ Tutorial2/Tutorial2/Program.cs
@@ -6,8 +6,8 @@ StatisticsHelper s = new StatisticsHelper();
 
 Console.WriteLine("StatisticsHelper created");
 
-int[] val = [2,3];
-Console.WriteLine(CalculateAverage(val));
+int[] val1 = [2,3];
+Console.WriteLine(CalculateAverage(val1));
 
 
 float CalculateAverage(int[] values)
